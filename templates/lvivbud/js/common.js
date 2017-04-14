2a7d975aa60a391350dc4e704b854ac009fd366b
dm <polowesa>
2017-04-14 18:42:55 +0300

    scroll down

--- templates/lvivbud/js/common.js
+++ templates/lvivbud/js/common.js
@@ -18,6 +18,17 @@ jQuery(document).ready(function($) {
         $('.dropdown').toggleClass('open');
     });
 
+    $('#scroll').click("#services", function (event) {
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 600);
+
+    } );
+
 
 //  =====================   sliders   =================================
 
