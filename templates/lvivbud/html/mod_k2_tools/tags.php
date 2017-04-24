da50eb92e91c8e48f3e90129a24e16f41b5625ff
dm <polowesa>
2017-04-24 18:48:41 +0300

    delete underline, translate form validation errors, add new "all projects" tag button

--- templates/lvivbud/html/mod_k2_tools/tags.php
+++ templates/lvivbud/html/mod_k2_tools/tags.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 ?>
 
 <div id="k2ModuleBox<?php echo $module->id; ?>" class="works_tag k2TagCloudBlock<?php if($params->get('moduleclass_sfx')) echo ' '.$params->get('moduleclass_sfx'); ?>">
+	<a href="/vykonani-roboty" class="works_tag__item" > <?php echo JText::_('MOD_K2_ALL_RROJECTS') ?></a>
 	<?php foreach ($tags as $tag): ?>
 	<?php if(!empty($tag->tag)): ?>
 	<a href="<?php echo $tag->link; ?>" class="works_tag__item" title="<?php echo $tag->count.' '.JText::_('K2_ITEMS_TAGGED_WITH').' '.K2HelperUtilities::cleanHtml($tag->tag); ?>">
