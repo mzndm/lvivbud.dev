fbd4a0122273bf8ea9f43a1659ecf3150a1b65c0
dm <polowesa>
2017-04-26 17:54:20 +0300

    add gulp-autoprefixer, add gulp-cssmin

--- lvivbud_gulp/gulpfile.js
+++ lvivbud_gulp/gulpfile.js
@@ -1,6 +1,8 @@
  var gulp 		= require('gulp'),
 	 sass 		= require('gulp-sass'),
- 	 sourcemaps = require('gulp-sourcemaps');
+ 	 sourcemaps = require('gulp-sourcemaps'),
+ 	 cssmin     = require('gulp-cssmin'),
+ 	 prefixer   = require('gulp-autoprefixer');
 	// browserSync = require('browser-sync'),
 	// concat 		= require('gulp-concat'),
 	// uglify   	= require('gulp-uglifyjs');
@@ -9,6 +11,8 @@ gulp.task ('sass', function() {
 	return gulp.src('app/sass/main.scss')
 		.pipe(sourcemaps.init())
     	.pipe(sass())
+		.pipe(prefixer())
+		.pipe(cssmin())
     	.pipe(sourcemaps.write())
     	.pipe(gulp.dest('../templates/lvivbud/css'));
 });
